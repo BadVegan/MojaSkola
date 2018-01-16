a83253e07c2d5741efb8c14568e01b280a891d92
BadVegan <davit@wp.pl>
2018-01-16 17:45:12 +0100

    dodano redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
+import {createStore} from 'redux';
+import reducer from './store/reducer';
+import {Provider} from 'react-redux';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
 );
 registerServiceWorker();
