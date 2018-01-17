e01927447a5ee9e59645d20f75dcd7f0d9ed7b51
BadVegan <davit@wp.pl>
2018-01-17 20:54:57 +0100

    dodano redux thunk

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter} from 'react-router-dom';
-import {createStore} from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './store/reducer';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-const store = createStore(reducer);
+const store = createStore(reducer,applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App/>
+            <App />
         </BrowserRouter>
     </Provider>,
     document.getElementById('root')
